a8e4d05e1dfaf6db531c5907a1b75094be4c19fc
DaiYu-233 <103192865+DaiYu-233@users.noreply.github.com>
2023-12-02 03:44:23 +0000

    Update Updated By Github Actions

--- js/debounce.js
+++ js/debounce.js
@@ -1,7 +1,1 @@
-// 防抖全局计时器
-let TT = null;    //time用来控制事件的触发
-// 防抖函数:fn->逻辑 time->防抖时间
-function debounce(fn, time) {
-    if (TT !== null) clearTimeout(TT);
-    TT = setTimeout(fn, time);
-}
+let TT=null;function debounce(T,e){null!==TT&&clearTimeout(TT),TT=setTimeout(T,e)}
