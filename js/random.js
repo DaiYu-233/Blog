595bbff0be4cc3388a539f8b7b79e9b86f8c72cc
DaiYu-233 <103192865+DaiYu-233@users.noreply.github.com>
2023-12-17 07:34:18 +0000

    1 Updated By Github Actions

--- js/random.js
+++ js/random.js
@@ -1,1 +1,1 @@
-var posts=["posts/dd14.html","posts/da27.html","posts/d53b.html","posts/a5ca.html"];function toRandomPost(){window.open("/"+posts[Math.floor(Math.random()*posts.length)],"_self")}
+var posts=["posts/dd14.html","posts/d53b.html","posts/da27.html","posts/a5ca.html"];function toRandomPost(){window.open("/"+posts[Math.floor(Math.random()*posts.length)],"_self")}
